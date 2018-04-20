e34bdc251d4c7a681d627e19d6f7f4b21262a72e
Viral Solani <cygnet.vhsolani@gmail.com>
2018-04-20 09:05:28 +0530

    Component Architecture and Feature Modules

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `
 })
-export class AppComponent {
-  title: string;
-  constructor() {
-    this.title = 'Ultimate Angular';
-  }
-}
+export class AppComponent {}
