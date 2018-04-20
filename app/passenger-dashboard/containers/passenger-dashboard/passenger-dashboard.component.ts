c6284b6b38827745c99f5e38ddbcccaf1246f710
Viral Solani <viral.solani@gmail.com>
2018-04-20 10:15:22 +0530

    - immutable state changes - implement ngOnchanges life cycle hook for one way data binding

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -10,6 +10,9 @@ import { Passenger } from '../../models/passenger.interface';
       <passenger-count
         [items]="passengers">
       </passenger-count>
+      <div *ngFor="let passenger of passengers;">
+        {{ passenger.fullname }}
+      </div>
       <passenger-detail
         *ngFor="let passenger of passengers;"
         [detail]="passenger"
@@ -55,10 +58,17 @@ export class PassengerDashboardComponent implements OnInit {
       children: null
     }];
   }
-  handleEdit(event) {
-    console.log(event);
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    });
   }
-  handleRemove(event) {
-    console.log(event);
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
   }
 }
