07c9a592293f4036a99b6f4e4c25c5e10ab2b5a6
Viral Solani <viral.solani@gmail.com>
2018-04-24 11:02:17 +0530

    - Custom Headers and RequestOptions - Observable.catch error handling

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -3,6 +3,8 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Passenger } from './models/passenger.interface';
 
@@ -15,19 +17,28 @@ export class PassengerDashboardService {
   getPassengers(): Observable<Passenger[]> {
     return this.http
       .get(PASSENGER_API)
-      .map((response: Response) => response.json());
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
   updatePassenger(passenger: Passenger): Observable<Passenger> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({
+      headers: headers
+    });
     return this.http
-      .put(`${PASSENGER_API}/${passenger.id}`, passenger)
-      .map((response: Response) => response.json());
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http
       .delete(`${PASSENGER_API}/${passenger.id}`)
-      .map((response: Response) => response.json());
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
   }
 
 }
